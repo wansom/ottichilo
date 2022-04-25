f94c970fd8725107e5bf3bd94a00cfa014f69f44
wansom <ochiengwarren10@gmail.com>
2022-04-25 10:02:06 +0300

    add build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    space: 'wl1z0pal05vy',
+  accessToken:
+    '0e3ec801b5af550c8a1257e8623b1c77ac9b3d8fcfc1b2b7494e3cb77878f92a',
+
+  contentTypeIds: {
+    product: '2PqfXUJwE8qSYKuM0U6w8M',
+  },
+  }
 };
