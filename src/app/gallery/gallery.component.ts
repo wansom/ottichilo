ae2ee4ed96e0fa209bb5f58aee3cec97497f3c87
wansom <ochiengwarren10@gmail.com>
2022-05-13 13:25:03 +0300

    5/13/2022

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CrmService } from '../services/crm.service';
 
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
