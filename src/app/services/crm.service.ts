4bf4e37e8b346d2048d08cb27f9612e4bcaa834b
wansom <ochiengwarren10@gmail.com>
2022-05-17 20:00:54 +0300

    add date

--- src/app/services/crm.service.ts
+++ src/app/services/crm.service.ts
@@ -23,25 +23,26 @@ export class CrmService {
       content_type: environment.contentful.contentTypeIds.blogpost,
     })
     .then((entry) => {
+      console.log("entry",entry.items)
       let posts = entry.items.map((item:any) => {
-        const { title, date,description,  }:any = item.fields;
+        const { title, date,description,categories  }:any = item.fields;
         const { id } = item.sys;
         const image:any = item.fields.image.fields.file.url;
-        return { title,date, description, id, image };
+        return { title,date, description,categories, id, image };
       });
      return posts
     });
   }
   getImages(){
     return this.client.getEntries({
-      content_type: "gallery",
+      content_type: "pictures",
     })
     .then((entry) => {
       let posts = entry.items.map((item:any) => {
-        const { title,description,category  }:any = item.fields;
+        const { title,description,category,date  }:any = item.fields;
         const { id } = item.sys;
         const image:any = item.fields.image.fields.file.url;
-        return { title, description, category,id, image };
+        return { title, description, category,date,id, image };
       });
      return posts
     });
