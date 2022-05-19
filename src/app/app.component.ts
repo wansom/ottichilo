973335e678367782985be59050fbd3db2d2fbc11
wansom <ochiengwarren10@gmail.com>
2022-05-19 12:50:28 +0300

    config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DomService } from './services/dom.service';
-
+import {Router, NavigationEnd} from "@angular/router"
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +9,20 @@ import { DomService } from './services/dom.service';
 })
 export class AppComponent {
   title = 'ottichilo';
-  constructor(private rightClickDisable: DomService) {}
+  constructor(private rightClickDisable: DomService, private router:Router) {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+          gtag('config', 'UA-229327585-1', 
+                {
+                  'page_path': event.urlAfterRedirects
+                }
+               );
+       }
+    }
+ )}
+  
   ngOnInit() {
     this.rightClickDisable.disableRightClick();
   }
+
 }
